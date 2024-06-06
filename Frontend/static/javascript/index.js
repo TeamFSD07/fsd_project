2018767652e53d4503f8f1425bb801ba0afb6eeb
Team_FSD07 <singhkk8899@gmail.com>
2024-06-06 17:29:48 +0530

    admin dashboard  and inventory

--- Frontend/static/javascript/index.js
+++ Frontend/static/javascript/index.js
@@ -1,23 +1,6 @@
-const primaryHeader = document.querySelector(".primary-header");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".primary-navigation");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
 
-navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expanded", false)
-    : navToggle.setAttribute("aria-expanded", true);
-  primaryNav.toggleAttribute("data-visible");
-  primaryHeader.toggleAttribute("data-overlay");
-});
-
-const slider = new A11YSlider(document.querySelector(".slider"), {
-  adaptiveHeight: false,
-  dots: true,
-  centerMode: true,
-  arrows: false,
-  responsive: {
-    480: {
-      dots: false, // dots enabled 1280px and up
-    },
-  },
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
 });
